a4f2e197d8993a4b60b2e4c8bf6f33e0da2e1bb2
nameischandu09 <140332143+nameischandu09@users.noreply.github.com>
2024-05-03 20:33:50 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,89 @@
+let allMusic = 
+[
+    {
+        name: "Calm-Down",
+        artist: "Rema, Selena Gomez",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Let Me Down Slowly",
+        artist: "Alec Benjamin",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Love Me Like You Do",
+        artist: "Ellie Goulding",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Starboy",
+        artist: "The Weeknd",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Girls Like You",
+        artist: "Maroon 5",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "On My Way",
+        artist: "Alan Walker",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Sorry",
+        artist: "Justin Beiber",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Sugar and Brownies",
+        artist: "Dharia",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Faded",
+        artist: "Alan Walker",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name:" Govinda Namalu",
+        artist:"SPB",
+        img:"music-3",
+        src:"music-11"
+    },
+    {
+        name:"Good Night",
+        artist:"Me",
+        img:"music-12",
+        src:"music-12",
+    },
+    {
+        name:"Agnathavasi",
+        artist:"AR",
+        img:"music-13",
+        src:"music-13",
+
+    },
+    {
+        name:"kabali",
+        artist:"SSR",
+        img:"music-14",
+        src:"music-14",
+    },
+    {
+        name:"Ganapathi Song",
+        artist:"SS",
+        img:"music-15",
+        src:"music-15",
+    }
+
+];
